98dfec32668ac7858d284dc5d54660bb2974a51b
Omer Karisman <ok@okaris.com>
2018-02-05 16:38:52 +0300

    Fix port setting for Heroku

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ server.get('/', (req, res, next) => {
 
 server.post('/', mainController.home);
 
-server.listen(8080, function() {
+var port = process.env.PORT || 8080;
+server.listen(port, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
